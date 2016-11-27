cc7b165b0f2750c25fdeb920e2d7d84d87e9f0b1
Gabriel Sandoval <gfsandovalv@unal.edu.co>
2016-11-27 17:37:13 -0500

    ahora se imprime en un archivo *.dat

--- stormer1.cpp
+++ stormer1.cpp
@@ -1,24 +1,29 @@
 #include "stormer.h"
+#include<fstream>
+
+void printVector(vectorR4 a);
 int main(){
   vectorR4 STR, A; //vetores stormer y Condiciones iniciales
   A.r1=R0;   A.r2=z0;  A.r3=R10;  A.r4=z10;
-  double h=0.001;
+  double h=0.0001;
   //intervalo [a , b] , n = (b-a)/h
   int n=(0.3 - 0)/h;
   double  phi_n=0.0,phi_n1=0.0;
-  
-  //  std::cout<< "R" << "\t" << "z" << std::endl;
-    
+
+  //R,z,phi
+//se implementa el método y se guardan los datos en "trajectory.dat"
+  std::ofstream file;	
+  file.open("trajectory.dat");
   for (int a=0;a<n;a++){
     STR=stormer(R2,z2,h,n,A);
     phi_n1=integ(h,phi1,STR,phi_n);
-    printVector(A);
-    std::cout<<"  "<<phi_n1<<std::endl;
+    file<<std::setprecision(9)<<std::fixed<< A.r1 << "  " << A.r2<<"  "<<phi_n1<<std::endl;
     phi_n=phi_n1;
     A=STR;
   }
-
+    file.close();
   return 0;
 }
+
 
 
